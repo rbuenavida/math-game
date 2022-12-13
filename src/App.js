7e8fa02d28d6f8b85eb9a471c14a34c4dfae76a6
Ron Buenavida <rbuenavida@gmail.com>
2022-12-13 02:19:04 -0500

    App is now a functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 // import logo from './logo.svg';
 import styles from './App.module.css';
 
@@ -9,46 +9,79 @@ import MultipleChoice from './components/MultipleChoice';
 
 const TIME = 30; // Initial game duration, in seconds
 
-class App extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    
-    const status = this.getInitialStatus();
-    
-    this.state = {
-      status,
-      selected: -1,
-      showSummary: false,
-      endTime: Date.now() + TIME*1000,
-      prev: this.generateProblem(status.max),
-      next: this.generateProblem(status.max)
-    };
+const App = () => {
+  const initialStatus = {
+    score: 0,
+    scoreChange: 0,
+    max: 10,
+    asked: 0,
+    answered: 0,
+    multiplier: 1
+  }
+
+  const shuffle = (a) => {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a
   }
   
-  getInitialStatus() {
-    return {
-      score: 0,
-      scoreChange: 0,
-      max: 10,
-      asked: 0,
-      answered: 0,
-      multiplier: 1
-    };
+  const getChoices = (a, b, max) => {
+    const result = a + b
+    const choices = [result]
+    
+    while(choices.length < 4) {
+      const choice = randomNumber(max)
+      if(!choices.includes(choice)) {
+        choices.push(choice)
+      }
+    }
+    return shuffle(choices)
   }
-  
-  randomNumber(max) {
+
+  const randomNumber = (max) => {
     return Math.floor(Math.random()*max);
   }
   
-  generateProblem(max) {
-    const a = this.randomNumber(max);
-    const b = this.randomNumber(max - a);
-    return {a, b, choices: this.getChoices(a, b, max)};
+  const generateProblem = (max) => {
+    const a = randomNumber(max)
+    const b = randomNumber(max - a)
+    return { a, b, choices: getChoices(a, b, max) }
   }
-  
-  increaseScore() {
-    const {status} = this.state;
-    let endTime = this.state.endTime;
+
+  const [ state, setState ] = useState({
+    status: { ...initialStatus },
+    selected: -1,
+    showSummary: false,
+    endTime: Date.now() + TIME * 1000,
+    prev: generateProblem(initialStatus.max),
+    next: generateProblem(initialStatus.max)
+  })
+
+  useEffect(() => {
+    if (state.status.scoreChange !== 0) {
+      setTimeout(() => {
+        console.log('settimeout state', state)
+        const { status, next } = state
+        setState({
+          ...state,
+          status: {
+            ...status,
+            scoreChange: 0
+          },
+          prev: next,
+          next: generateProblem(status.max),
+          selected: -1
+        });
+      }, 1500)
+    }
+  }, [state])
+
+  const increaseScore = (selected) => {
+    console.log('increaseScore')
+    const { status } = state;
+    let endTime = state.endTime;
     let max = status.max;
     
     // Add time and increase max every 5 correct answers
@@ -60,7 +93,9 @@ class App extends React.PureComponent {
     const score = status.score + (max * status.multiplier)
     const scoreChange = score - status.score
 
-    this.setState({
+    setState({
+      ...state,
+      selected,
       endTime,
       status: {
         ...status,
@@ -71,16 +106,20 @@ class App extends React.PureComponent {
         answered: status.answered + 1,
         max
       }
-    });
+    })
   }
   
-  decreaseScore() {
-    const { status } = this.state;
+  const decreaseScore = (selected) => {
+    console.log('decreaseScore')
+    const { status } = state;
 
-    const score =  Math.max(0, status.score - Math.floor(status.max*0.25)) // Decrease by MAX * <Correct Answer Probability>
+    // Decrease by MAX * <Correct Answer Probability>
+    const score =  Math.max(0, status.score - Math.floor(status.max * 0.25)) 
     const scoreChange = score - status.score
     
-    this.setState({
+    setState({
+      ...state,
+      selected,
       status: {
         ...status,
         multiplier: 1,
@@ -90,113 +129,79 @@ class App extends React.PureComponent {
       }
     }); 
   }
-  
-  shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
-  }
-  
-  getChoices(a, b, max) {
-    const result = a + b;
-    const choices = [result];
+
+  const handleOnClick = value => {
+    const { prev: {a, b}, selected } = state
     
-    while(choices.length < 4) {
-      const choice = this.randomNumber(max);
-      if(!choices.includes(choice)) {
-        choices.push(choice);
-      }
-    }
-    return this.shuffle(choices);
-  }
-  
-  handleOnClick = value => {
-    const {prev: {a, b}, selected} = this.state;
-    
-    if(selected !== -1) return;
+    if(selected !== -1) return
     
     if(value === a + b) {
-      this.increaseScore();
+      increaseScore(value)
     }
     else {
-      this.decreaseScore();
+      decreaseScore(value)
     }
-    
-    this.setState({selected: value});
-    
-    setTimeout(() => {
-      const { status } = this.state;
+  }
 
-      this.setState({
-        status: {
-          ...status,
-          scoreChange: 0
-        },
-        prev: this.state.next,
-        next: this.generateProblem(this.state.status.max),
-        selected: -1
-      });
-    }, 1500);
-  };
-
-  handleOnTimerEnd = () => {
-    this.setState({showSummary: true});
+  const handleOnTimerEnd = () => {
+    setState({
+      ...state,
+      showSummary: true
+    })
   }
   
-  handleOnPlayAgain = () => {
-    const status = this.getInitialStatus();
-    this.setState({
+  const handleOnPlayAgain = () => {
+    const status = { ...initialStatus };
+    setState({
+      ...state,
       status,
       showSummary: false,
       endTime: Date.now() + TIME*1000,
-      prev: this.generateProblem(status.max),
-      next: this.generateProblem(status.max)
-    });
+      prev: generateProblem(status.max),
+      next: generateProblem(status.max)
+    })
   }
-  
-  render() {
-    const {
-      prev, 
-      next, 
-      status, 
-      selected, 
-      showSummary, 
-      endTime
-    } = this.state;
 
-    return (
-      <div className={styles.gameContainer}>
-        <Header 
-          status={status} 
-          endTime={endTime} 
-          onTimerEnd={this.handleOnTimerEnd}/>
-        <div className={styles.body}>
-          <Expression 
-            from={`${prev.a} + ${prev.b} = `} 
-            to={`${next.a} + ${next.b} = `} 
-            transitioning={selected !== -1}
-          />
-        </div>
-        <div className={styles.footer}>
-          <div className={styles.footerContainer}>
-            <MultipleChoice 
-              values={prev.choices} 
-              selected={selected} 
-              onClick={this.handleOnClick} 
-              correct={selected === prev.a + prev.b}
-            />
-          </div>
-        </div>
-        <Summary 
-          show={showSummary} 
-          score={status.score} 
-          onPlayAgain={this.handleOnPlayAgain}
+  const {
+    prev, 
+    next, 
+    status, 
+    selected, 
+    showSummary, 
+    endTime
+  } = state;
+
+  return (
+    <div className={styles.gameContainer}>
+      <Header 
+        status={status} 
+        endTime={endTime} 
+        onTimerEnd={handleOnTimerEnd}/>
+      <div className={styles.body}>
+        <Expression 
+          from={`${prev.a} + ${prev.b} = `} 
+          to={`${next.a} + ${next.b} = `} 
+          transitioning={selected !== -1}
         />
       </div>
-    );
-  }
+      <div className={styles.footer}>
+        <div className={styles.footerContainer}>
+          <MultipleChoice 
+            values={prev.choices} 
+            selected={selected} 
+            onClick={handleOnClick} 
+            correct={selected === prev.a + prev.b}
+          />
+        </div>
+      </div>
+      <Summary 
+        show={showSummary} 
+        score={status.score} 
+        onPlayAgain={handleOnPlayAgain}
+      />
+    </div>
+  );
+  
 }
 
 export default App;
